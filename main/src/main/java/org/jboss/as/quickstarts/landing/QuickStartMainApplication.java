6c92b1985a7a4e50feafcbfed75fb5ca72f7fc6d
= <=>
2024-10-21 14:53:06 -0400

    Version with spring boot and java additions

---
+++ main/src/main/java/org/jboss/as/quickstarts/landing/QuickStartMainApplication.java
@@ -0,0 +1,13 @@
+package org.jboss.as.quickstarts.landing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartMainApplication.class, args);
+	}
+
+}
---
+++ main/src/test/java/org/jboss/as/quickstarts/landing/QuickStartMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jboss.as.quickstarts.landing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
